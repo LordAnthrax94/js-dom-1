1fd5eba8e75cc4fbb517bf0143289dc2f6cf38f3
LordAnthrax94 <andreagi1994@gmail.com>
2024-11-21 15:28:37 +0100

    fix: bonus e css

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const lampadinaOff = document.querySelector('.spenta')
 const lampadinaOn = document.querySelector('.accesa')
 
-const stanza = document.querySelector('.lampadario')
+const stanza = document.querySelector('.container')
 const bottone = document.getElementById('interruttore')
 
 
 
 bottone.addEventListener ('click', () =>{
   lampadinaOff.classList.toggle('d-hide');
-  lampadinaOn.classList.toggle('d-hide'); 
+  lampadinaOn.classList.toggle('d-hide');
+  stanza.classList.toggle('bg-color')
   bottone.innerHTML = (lampadinaOff.classList.contains('d-hide')) ? 'Spegni' : 'Accendi'
 })
